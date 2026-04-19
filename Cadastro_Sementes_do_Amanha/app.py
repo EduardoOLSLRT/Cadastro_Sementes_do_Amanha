8b6770c434a395c1d2f2917e3d155bfcd2f64fc3
João Morais <jv.timotio@gmail.com>
2026-04-19 10:33:24 -0300

    Criando templates restantes

--- Cadastro_Sementes_do_Amanha/app.py
+++ Cadastro_Sementes_do_Amanha/app.py
@@ -5,14 +5,24 @@ from app import create_app
 
 load_dotenv()
 
-# Aqui está o segredo: dizemos ao Flask exatamente onde a pasta templates está
 app = create_app()
 app.template_folder = os.path.join(os.getcwd(), 'templates')
 
-@app.route('/emitir-docs')
-def pagina_emissao():
-    # O nome aqui deve ser igual ao arquivo na sua pasta templates
+@app.route('/emitir-uso-imagem')
+def pagina_uso_imagem():
     return render_template('termo_uso_de_imagem.html')
+
+@app.route('/emitir-ficha-acolhimento')
+def pagina_ficha():
+    return render_template('ficha_de_acolhimento.html')
+
+@app.route('/emitir-responsabilidade')
+def pagina_responsabilidade():
+    return render_template('termo_de_responsabilidade.html')
+
+@app.route('/emitir-saida')
+def pagina_saida():
+    return render_template('termo_de_autorizacao_saida.html')
 
 if __name__ == "__main__":
     host = os.getenv("HOST", "127.0.0.1")
